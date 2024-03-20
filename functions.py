f9308c1813df4577cfef956b4d50064d5f8aef98
Hormone <sach.laur.04@protonmail.com>
2024-03-20 17:56:47 +0100

    Fixed display automaton function

--- functions.py
+++ functions.py
@@ -1,7 +1,17 @@
 
 def display(M, L, E):   # L is symbols in the alphabet, E is list of initial/ final states (in order)
-    #m = len(str(max(map(max,M))))
-    m = 4
+    contains_list = False
+    m = 0
+    for i in A:
+        for j in i:
+            if isinstance(j, list):
+                contains_list = True
+                m = max(m, len(str(j)))
+    m -= 2
+
+    if contains_list == False:
+        m = len(str(max(map(max,A))))
+    
     print(" "*(m*2+6),end="")
     for i in range(len(M[0])-1):
         print(L[i],end=" "*(m+2))   # print the symbols in the alphabet
@@ -10,7 +20,7 @@ def display(M, L, E):   # L is symbols in the alphabet, E is list of initial/ fi
     for i in range(len(M)) :
         print(E[i],end="")   # print initial or final
         for j in range(len(M[0])):
-            if isinstance(M[i][j], tuple):   # if the element is a tuple
+            if isinstance(M[i][j], list):
                 print(a.format(" "+str(M[i][j])[1:-1]),end="")
             else:
                 if M[i][j] == -1:
@@ -27,27 +37,33 @@ def display(M, L, E):   # L is symbols in the alphabet, E is list of initial/ fi
 
 
 
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     # TEST
     # L is symbols in the alphabet, E is list of initial/ final states (in order)
     L = ["a","b","c","d"]
     E = [" ->", "   ", "<->", "<- "]
-    A = [[0, 1, (2,3), 3, 4],
+    A = [[0, 1, [2,3], 3, 4],
          [2, 2, 2, 3, 1],
          [3, 4, 2, 3, 4],
          [4, 4, 4, 3, 4]]
-
-
-
-    print()
     display(A, L, E)
 
     L = ["a","b"]
     E = [" ->", "   ", "   ", "   ", "<- "]
-    A = [[0, (0,1), 0],
+    A = [[0, [0,1,8,9], 0],
         [1, -1, 2],
         [2, 3, -1],
         [3, 4, -1],
         [4, -1, -1]]
-    
     display(A, L, E)
+
+    a = input("Press enter to exit")
