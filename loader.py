48600b46e2d14a14308a077bb2c37e5482f50a7e
Chevroum <101296753+Chevroum@users.noreply.github.com>
2024-03-27 17:57:22 +0100

    text to command
    incredible transcription

--- loader.py
+++ loader.py
@@ -1,14 +1,57 @@
 # Loader to extract the data from the text file
 
 
-# def load_data(file):
+def load_data(file):
+
+     mylines = []
+     with open (file, 'rt') as myfile:                                   # Open the file given and reads it
+         for myline in myfile:                                           # For each line, stored as myline
+             mylines.append(myline.replace('\n', ''))        # add its contents to mylines and replaces the next line character by nothing
+     return mylines
 
 
+def load_language(data):
+    L = []
+
+    for i in range(5, len(data)):
+        if data[i][1] not in L:
+            L.append(data[i][1])
+    return L
 
 
+def load_state(data):
+
+    E = []
+
+    for i in range(0, ord(data[1]) - 48):                               #convert string to number
+        if str(i) in (data[2][1:]) and str(i) in (data[3][1:]):
+            E.append("<->")
+        elif str(i) in (data[2][1:]):
+            E.append(" ->")
+        elif str(i) in (data[3][1:]):
+            E.append("<- ")
+        else:
+            E.append("   ")
+    return E;
 
 
+def load_transition(data):
 
+    A = [[-1 for j in range(len(L) + 1)] for i in range((ord(data[1]) - 48))]
+
+    for i in range(ord(data[1]) - 48):
+        A[i][0] = i
+
+    for i in data[5:]:
+
+        if A[ord(i[0]) - 48][ord(i[1]) - 96] == -1:
+            A[ord(i[0]) - 48][ord(i[1]) - 96] = ord(i[2]) - 48
+        else:
+            if not isinstance(A[ord(i[0]) - 48][ord(i[1]) - 96], list):
+                A[ord(i[0]) - 48][ord(i[1]) - 96] = [A[ord(i[0]) - 48][ord(i[1]) - 96]]
+            A[ord(i[0]) - 48][ord(i[1]) - 96].append(ord(i[2]) - 48)
+
+    return A
 
 
 # Example of the text file:
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # Mainloop
+from loader import *
+
+data = load_data('test_automata.txt')  #import data
+print(data)
+
+
+#create the language
+
+L = load_language(data)
+print(L)
+
+#terminal and non terminal matrice
+E = load_state(data)
+print(E)
+
+#transition matrice
+
+A = load_transition(data)
+print(A)
+
