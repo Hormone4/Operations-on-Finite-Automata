2d4631509fb76462099eeee3b9695e0e4e7c41c7
Hormone <sach.laur.04@protonmail.com>
2024-04-26 12:09:19 +0200

    execution-trace-maker now removes empty lines

--- execution-trace-maker.py
+++ execution-trace-maker.py
@@ -11,6 +11,15 @@ automaton_files = [f for f in files if f.endswith(".txt") and not(f.startswith("
 # For each automaton file
 for automaton_file in automaton_files:
     print("\n\n\n AUTOMATON FILE:",automaton_file)
+
+    # Remove empty lines from the automaton file
+    with open(automaton_file, 'r') as file:
+        lines = file.read().splitlines()  # this does not include the newline characters
+        lines = [line for line in lines if line.strip() != '']
+    with open(automaton_file, 'w') as file:
+        file.write('\n'.join(lines))
+
+
     # Construct the command to run the python script
     command = f"python test.py {automaton_file}"
 
