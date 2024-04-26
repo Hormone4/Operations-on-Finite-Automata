040c454a9c64bbff2ac4adbef9259d0733857b8d
Hormone <sach.laur.04@protonmail.com>
2024-04-26 12:12:46 +0200

    Added renamer

---
+++ renamer.py
@@ -0,0 +1,26 @@
+# the folder contains 44 text files named as: Int3-2-44.txt, Int3-2-43.txt, Int3-2-42.txt...
+# the goal is to rename the files as: Int1-2-44.txt, Int1-2-43.txt, Int1-2-42.txt...
+
+
+
+import os
+
+# Define the directory
+dir_path = r'C:\Users\sachl\Documents\GitHub\Operations-on-Finite-Automata'  # replace with your directory path
+
+# Get list of all files
+files = os.listdir(dir_path)
+
+# Iterate over each file
+for file in files:
+    # Check if the file starts with 'Int3'
+    if file.startswith('Int1-2-'):
+        # Construct new filename
+        new_filename = file.replace('Int1-2-', '')
+        
+        # Get full file paths
+        old_file_path = os.path.join(dir_path, file)
+        new_file_path = os.path.join(dir_path, new_filename)
+        
+        # Rename the file
+        os.rename(old_file_path, new_file_path)
